2091675c9e798c91d759f4b425e5219b5cd43dc6
Hamed Karami <hkarami8@gmail.com>
2019-10-17 15:13:15 +0330

    اضافه شدن مدل های BloodControl و BloodControlDetails

---
+++ LaboratoryQualityControl/Models/BloodControlDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LaboratoryQualityControl.Models2
+{
+    public partial class BloodControlDetails
+    {
+        public int BloodControlDetailsId { get; set; }
+        public int? BloodControlId { get; set; }
+        public decimal? Value { get; set; }
+        public int? RulesQcdetailsId { get; set; }
+        public string Description { get; set; }
+        public bool? Reject { get; set; }
+        public int? UserCode { get; set; }
+        public DateTime? LastUpdateTime { get; set; }
+        public DateTime? RecordTime { get; set; }
+
+        public virtual BloodControl BloodControl { get; set; }
+        public virtual RulesQcdetails RulesQcdetails { get; set; }
+        public virtual Users UserCodeNavigation { get; set; }
+    }
+}
