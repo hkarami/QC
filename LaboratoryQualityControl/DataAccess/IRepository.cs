4b719ac7379c135741c7b20fedee89a3a59d8ef0
Hamed Karami <hkarami8@gmail.com>
2019-10-17 12:48:42 +0330

    اضافه کردن کلاس Repository

---
+++ LaboratoryQualityControl/DataAccess/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LaboratoryQualityControl.DataAccess
+{
+    interface IRepository<TEntity> where TEntity : class, new()
+    {
+        #region [Methods]
+        TEntity GetById(object entityId);
+
+        void Insert(TEntity entity);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        #endregion
+        #region [Properties]
+        IQueryable<TEntity> Table { get; }
+        #endregion
+    }
+}
---
+++ LaboratoryQualityControl/DataAccess/Repository.cs
@@ -0,0 +1,62 @@
+﻿using LaboratoryQualityControl.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace LaboratoryQualityControl.DataAccess
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class, new()
+    {
+        #region [Fields]
+        private LaboratoryQCContext _context;
+        private DbSet<TEntity> _entities;
+        #endregion
+        #region [Ctor]
+        public Repository(LaboratoryQCContext context)
+        {
+            _context = context;
+            _entities = _context.Set<TEntity>();
+        }
+        #endregion
+
+        #region [Fiels]
+
+        public void Delete(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            _entities.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public TEntity GetById(Object entityId)
+        {
+            return _entities.Find(entityId);
+        }
+
+        public void Insert(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            _entities.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            _entities.Update(entity);
+            _context.SaveChanges();
+        }
+        #endregion
+
+        #region [Properties]
+        public IQueryable<TEntity> Table => _entities;
+
+        #endregion
+    }
+}
