e388d89a204034cd60d5b394589dd711c3cee9bb
akarami2017 <akarami2017@gmail.com>
2020-05-16 21:59:26 +0430

    اضافه کردن تغییرات در DevicesLis

--- LaboratoryQualityControl/ClientApp/src/components/Devices/DevicesList.js
+++ LaboratoryQualityControl/ClientApp/src/components/Devices/DevicesList.js
@@ -1,20 +1,24 @@
-import React, { Component } from 'react';
+﻿import * as React from 'react';
+//import { data } from '../../DataSource.js';
+import { DataManager, WebApiAdaptor, UrlAdaptor } from '@syncfusion/ej2-data';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Inject } from '@syncfusion/ej2-react-grids';
 
-export class DevicesList extends Component {
+//import ApiURL  from "../../Configs/App.Config";
+export default class DevicesList extends React.Component {
+    constructor() {
+      super(...arguments);
+      //this.hostUrl = ApiURL;
+      this.data = new DataManager({ url: "https://localhost:44372/api/device", adaptor: new WebApiAdaptor});
+    }
 
-  constructor (props) {
-    super(props);
-  }
 
-
-
-  render () {
-    return (
-      <div>
-        <h1>Devices List</h1>
-      </div>
-    );
+    render() {
+      return ( 
+        <GridComponent id="Grid" dataSource={this.data} ref={grid => this.gridInstance = grid} allowPaging={true} pageSettings={{ pageCount: 3 }}>
+          <ColumnsDirective>
+                <ColumnDirective field='deviceCode' width='100' textAlign="Right" />
+                <ColumnDirective field='deviceName' width='100' /> 
+            </ColumnsDirective>
+        </GridComponent> );
   }
 }
-
-export default DevicesList;
