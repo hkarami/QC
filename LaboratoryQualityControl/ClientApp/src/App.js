89ed9054af65fa814065b3259672ccd2f2fdf6d8
akarami2017 <akarami2017@gmail.com>
2020-05-16 22:09:25 +0430

    تغییر مربوط یه app.css

--- LaboratoryQualityControl/ClientApp/src/App.js
+++ LaboratoryQualityControl/ClientApp/src/App.js
@@ -1,10 +1,9 @@
-//import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
-
 import { Layout } from './components/Layout/Layout';
 import Routes from './components/Layout/Routes';
-
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 
 export default class App extends Component {
