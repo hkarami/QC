533044f2e44ad036a96e3d700eb5f378608960b8
akarami2017 <akarami2017@gmail.com>
2020-12-14 20:51:55 +0330

    add DateTimeService

---
+++ LaboratoryQualityControl/Services/DateTimeService/IDateTimeService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaboratoryQualityControl.Services.DateTimeService
+{
+    interface IDateTimeService
+    {
+        #region [Methods]
+        DateTime ToGeorgianDateTime(this string persianDate);
+        string ToPersianDateString(this DateTime georgianDate);
+        #endregion
+    }
+}
